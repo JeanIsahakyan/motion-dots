9c1673d894f5fe4216842d91d56bbbe3267aa5a4
Jean Isahakyan <jeanisahkyan@gmail.com>
2020-02-12 14:29:12 +0300

    Boolean param fixes

--- src/Type/BuiltinType.php
+++ src/Type/BuiltinType.php
@@ -15,6 +15,13 @@ class BuiltinType extends AbstractType implements TypeInterface {
      */
     public function build() {
         [$type, $value] = $this->field;
+        if ($type === 'boolean') {
+            if ($value === 'false' || $value === '0') {
+                $value = false;
+            } elseif ($type === 'true' || $value === '1') {
+                $value = true;
+            }
+        }
         settype($value, $type);
         return $value;
     }
