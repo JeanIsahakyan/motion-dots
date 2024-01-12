8e3fee4fd44defc107253cab31f83b56d448c22d
Alexandr Yermak <ayermak@protonmail.ch>
2024-01-12 03:24:40 +0300

    add readme, verbose output

--- src/Schema/Typescript/Generator.php
+++ src/Schema/Typescript/Generator.php
@@ -12,6 +12,8 @@ class Generator {
     'system',
   ];
 
+  private bool $is_verbose = true;
+
   public static function create(): self {
     return new self();
   }
@@ -23,18 +25,45 @@ class Generator {
     $schema = $response['response'];
     $separator = $processor->getSeparator();
 
+    $realpath = realpath($files_folder);
+    $this->echo("Generating Typescript schema in {$realpath}/");
+
     foreach ($schema as $space_schema) {
       $space_name = (string)$space_schema['name'];
       if ($this->isExcludedSpace($space_name)) {
         continue;
       }
       $space = new MethodsNode($space_schema, $separator);
-      FileWriter::write("{$files_folder}/{$space_name}.d.ts", $space->toString());
+      $file_name = "{$files_folder}/{$space_name}.d.ts";
+      $is_success = (bool)file_put_contents($file_name, $space->toString());
+      if (!$is_success) {
+        $this->echo("🥵  Error {$space_name} => {$file_name}");
+        break;
+      }
+      $this->echo("   {$space_name} => {$file_name}");
     }
+
+    $this->echo("✨ Done");
+  }
+
+  public function setIsVerbose(bool $is_verbose): self {
+    $this->is_verbose = $is_verbose;
+    return $this;
+  }
+
+  public function excludeSpaces(string ...$space_names): self {
+    $this->excluded_spaces = array_merge($this->excluded_spaces, $space_names);
+    return $this;
   }
 
   private function isExcludedSpace(string $space_name): bool {
     return in_array($space_name, $this->excluded_spaces);
+  }
+
+  private function echo(string $message): void {
+    if ($this->is_verbose) {
+      echo $message . "\n";
+    }
   }
 
 }
